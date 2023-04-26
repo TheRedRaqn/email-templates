850346c7b53caf4a36c07b474fb0c6eef727bfcf
Jan <stoker_jan@hotmail.com>
2023-04-26 16:33:05 +0200

    Fix path issue in listImages function

--- functions/listImages.js
+++ functions/listImages.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 exports.handler = async function (event, context) {
   const imagesDir = path.join(__dirname, '..', 'images');
-  const files = fs.readdirSync(imagesDir);
+  const files = fs.readdirSync(path.join(__dirname, '..', 'images'));
   const images = files.filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file));
 
   return {
