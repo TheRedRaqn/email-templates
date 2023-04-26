91800b89934ee390d11529505909f62c893809e1
Jan <stoker_jan@hotmail.com>
2023-04-26 16:40:15 +0200

    Update path for Netlify Functions environment

--- functions/listImages.js
+++ functions/listImages.js
@@ -1,9 +1,15 @@
+const os = require('os');
 const fs = require('fs');
 const path = require('path');
 
 exports.handler = async function (event, context) {
   const imagesDir = path.join(__dirname, '..', 'images');
-  const files = fs.readdirSync(path.join(__dirname, '..', 'images'));
+  const isLocal = os.platform() !== 'linux';
+	const imagePath = isLocal
+	  ? path.join(__dirname, '..', 'images')
+	  : path.join(process.env.LAMBDA_TASK_ROOT, '..', '..', 'images');
+
+	const files = fs.readdirSync(imagePath);
   const images = files.filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file));
 
   return {
