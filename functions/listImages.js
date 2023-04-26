15905ca84dc3060fdeebecadcbfd6e2497274297
Jan <stoker_jan@hotmail.com>
2023-04-26 16:48:42 +0200

    Update path for Netlify Functions environment

--- functions/listImages.js
+++ functions/listImages.js
@@ -1,22 +1,18 @@
-const os = require('os');
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
 
 exports.handler = async function (event, context) {
-  const imagesDir = path.join(__dirname, '..', 'images');
   const isLocal = os.platform() !== 'linux';
-	const imagePath = isLocal
-	  ? path.join(__dirname, '..', 'images')
-	  : path.join(process.env.LAMBDA_TASK_ROOT, '..', '..', 'images');
+  const imagePath = isLocal
+    ? path.join(__dirname, '..', 'public', 'images')
+    : path.join(process.env.LAMBDA_TASK_ROOT, '..', '..', 'public', 'images');
 
-	const files = fs.readdirSync(imagePath);
-  const images = files.filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file));
+  const files = fs.readdirSync(imagePath);
+  const imageFiles = files.filter((file) => file.match(/\.(jpg|jpeg|png|gif)$/));
 
   return {
     statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(images),
+    body: JSON.stringify(imageFiles),
   };
 };
